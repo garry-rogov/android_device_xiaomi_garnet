d00f4b3855b167b396a6522d594d356163bb47ee
garry-rogov <garry.rogov@gmail.com>
2024-08-09 12:50:40 +0300

    device: garnet: Setup overlays for XIG05
    Co-Authored-By: AdarshGrewal <adarshgrewal@gmail.com>

--- device.mk
+++ device.mk
@@ -418,12 +418,14 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayGarnetPoco \
     SettingsProviderOverlayGarnetRedmi \
     SettingsProviderOverlayGarnetRedmiCN \
+    SettingsProviderOverlayGarnetXIG05 \
     SystemUIOverlayGarnet \
     TelephonyOverlayGarnet \
     WifiOverlayGarnet \
     WifiOverlayGarnetPoco \
     WifiOverlayGarnetRedmi \
-    WifiOverlayGarnetRedmiCN
+    WifiOverlayGarnetRedmiCN \
+    WifiOverlayGarnetXIG05
 
 # Overlays - RU translations
 DEVICE_PACKAGE_OVERLAYS += \
