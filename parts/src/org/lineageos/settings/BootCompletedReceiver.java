c9478ae24c1faea0ba05734c6cdf9ea968be91de
garry-rogov <garry.rogov@gmail.com>
2024-08-16 11:21:58 +0300

    garnet: parts: Override HDR types for dolby vision

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -38,7 +38,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         // Override HDR types
         final DisplayManager displayManager = context.getSystemService(DisplayManager.class);
         displayManager.overrideHdrTypes(Display.DEFAULT_DISPLAY, new int[]{
-                HdrCapabilities.HDR_TYPE_HDR10,
+                HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
                 HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
 }
