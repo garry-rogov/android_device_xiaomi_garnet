9998f8eb7b3f3884c42a7ed0b64a5645fc37716c
garry-rogov <garry.rogov@gmail.com>
2025-02-25 22:33:38 +0300

    garnet:  add usb_compositions.conf

--- device.mk
+++ device.mk
@@ -570,7 +570,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
-    init.qcom.usb.sh
+    init.qcom.usb.sh \
+    usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
